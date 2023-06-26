5ab9f78917c11e26ef61bf3bdb26e99cb19ef344
ruhul <ra.01403288711@gmail.com>
2023-06-26 14:55:52 +0600

    File System vs DBMS

---
+++ dbms/FileVsDbms_3.sql
@@ -0,0 +1,29 @@
+Advantages of database management system over File System.
+
+1.Before Database system, there was fileSystem.Actually file system is in operating system
+and it cannot be shared to client system or server system. that is a big problem. In database system 
+the tables are shared among server and client both. From a Server all the data can be accessed
+from all over the world. That is why there is no boundaries.
+
+2.Quick searching
+
+3.In a big data specific small data can be gathered easily by writing query.
+
+4.meta data is a data abut a data.The data which describe about a specific data.
+Like the name of a file or size of a file or the location of a file etc.
+
+5.data is independent.
+
+6. Concurrency . Multiple person can access data by writing query at the same time.
+in Concurrency dbms has the protocol to access data but file system doesnot have.
+
+7.Security issue is very important.Role based access control is possible in Dbms .
+Like some of the authorized data can be accessed by student. Faculty can be accessed 
+some more data. lastly all the data can be accessed by the supreme authorty. that is possible in 
+database management system. but not possible in File System.
+
+8.Data redanduncy or data duplicaty . it can be reduced in DBMS. Like :
+my name is ruhul amin. this line can be written multiple time in a file system. which is a type of
+data repetation. Operating system does not check a sentence multiple time. but in database it can be reduced.
+Operating system may check a file is created multiple time or not but it does not check a data is written multiple
+time.
