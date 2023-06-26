6a85d9201ba3c0296b3ec1739e8ef6ea5c437dd4
ruhul <ra.01403288711@gmail.com>
2023-06-26 16:45:41 +0600

    three schema architecture

---
+++ dbms/three_schema_architecture_6.sql
@@ -0,0 +1,25 @@
+Schema means stracture.
+This three schema architecture was created in 1970 for 
+data independence. In this model user cannot be able to know
+where the location of a data.We are storing everything in Google Drive.
+but we dont know where the location is.
+
+in this architecture the data can be represented in 
+External Schema. 
+the relation among the schema is described in Conceptual schema.
+
+in physical schema the location of the database, the size of the database ,
+the meta data has been described inside physical schema.
+it describe, where the data is physically present!
+
+Frontend developer works in External Schema level
+The database designeer works in conceptual Schema level
+Database administrator works in the physical schema level. Besides the database administrator 
+can deside the data should be stored in distributed database or in centralized database system.
+
+
+
+View Level =>      External Schema
+Conceptual Schema =>   Logical Level
+Physical Schema =>  Physical Level 
+Database Server 
