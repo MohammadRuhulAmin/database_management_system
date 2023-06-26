58976ae3bc8e229c5796d848bd15c76797252a5e
ruhul <ra.01403288711@gmail.com>
2023-06-26 15:59:13 +0600

    database Architecture

---
+++ dbms/database_archetecture_4.sql
@@ -0,0 +1,20 @@
+2 Tier Architecture : means 2 layer (First layer is Client layer and the second layer is database)
+it is also called client server architecture.
+
+3 Tier Architecture: means 3 layers (
+    First layer is Client Layer (API layer or interface layer)
+    Second layer is Application layer
+    Third layer is database server layer 
+) 
+We write query in client / API interface then it will be processed in server then return the 
+expected output.
+
+in 2 tier architecture the the query was processing inside database server,
+but in 3 tier architecture the query will be processing inside Application / business layer.
+All the code will be converted to the application layer / business layer and send it to the database layer.
+But in 2 tire , the code was processing and converting to machine language first inside database server layer
+which cause a slow performance and scalability is not maintained. Secondly security issue is concerned in 2 layer
+also.
+
+
+
