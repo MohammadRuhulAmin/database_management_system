c2801c2cafc9ded8147aa0800379c9d82013ec8e
ruhul <ra.01403288711@gmail.com>
2023-06-26 13:04:25 +0600

    Topic covered in mysql / database management system

---
+++ dbms/intro_topic.sql
@@ -0,0 +1,57 @@
+-- Author Md Ruhul Amin
+-- Software Engineer (Business Automation Ltd.)
+
+Basic introduction-
+Archetecture :
+    1. 2 tire archetecture
+    2. 3 tire archetecture
+  **3. 3 Schema archetecture (3 level of Abstruction)[This topic is under Data independence]
+Data Model :
+    1. Network Data Model 
+    2. Heirarcical Data Model
+    3. Relational Data Model
+    4. Entity Relation Data Model (Blue print)[attribute type, type of relationship]
+    5. Object Oriented Data Model 
+
+Basics of Key:
+    1.Primary Key
+    2.Candidate Key 
+    3.Super Key 
+    4.Foreign Key 
+    5.Alternative Key 
+
+Normalization :
+    1.Closure Method (Helps to find the candidaate key)
+    2.Functional dependency
+        2.1 1NF
+        2.2 2NF
+        2.3 3NF
+        2.4 BCNF
+        2.5 4NF
+        2.6 5NF
+    3.Minimization of Functional Dependency 
+
+Trunsaction Control And Concurrency
+    1.ACID (atomicity, consistency, isolation, and durability)
+    2.Problem (R-W),(W-R),(W-W)
+    3.Conflict Serilizability
+    4.Schedule Recoverableity 
+    5.2 Phase locking (2PL) 
+    6.Timestamps ordering
+
+SQL 
+    1.DDL
+    2.DML
+    3.DCL
+    4.Constraints(primary key, unique key, check , default , not null )
+    5.Aggregate Function(max,min,avg)
+    6.JOINS
+    7.Nested Query(IN,NOT IN,ANY,ALL)
+    8.Indexing 
+        8.1 Primary
+        8.2 Cluster 
+        8.3 Secondary
+    9.Multi level(B Tree , B+ Tree)
+
+Relational Algebra
+
