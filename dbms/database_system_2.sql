d50d3b4c6e4aa5bb6af5dc46edf01bf0dde215d3
ruhul <ra.01403288711@gmail.com>
2023-06-26 14:17:22 +0600

    database system

---
+++ dbms/database_system_2.sql
@@ -0,0 +1,13 @@
+Database System has two parts:
+    1.Database (collaction of related data)
+        1.1 Stractured 1.2 Un Stractured 
+    2.DBMS 
+        1.SQL Server
+        2.Oracle 
+        3.MySql
+        4.DB2
+    
+Here Relation means table 
+We can store data in many form like Graph,document etc.
+but when we store data in database in a table form that time we call it as relational 
+database management system.
