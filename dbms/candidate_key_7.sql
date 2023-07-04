08d28d2d0f7711eaa50dea94c12425b8ba72d323
ruhul <ra.01403288711@gmail.com>
2023-07-04 17:59:28 +0600

    candidate key

---
+++ dbms/candidate_key_7.sql
@@ -0,0 +1,15 @@
+Key :
+    it is an attribute. It works for identifying any two tuples in the table.
+    Why use key ? 
+    => to identify multiple rows in a table.
+
+    What is candidate key ? 
+    Candidate key is a single key or a group of multiple keys that uniquely identify rows in a table. 
+    A candidate key is a column or a combination of columns that uniquely identifies each row in a table. 
+    It is used to ensure that there are no duplicate or ambiguous records in a table.
+
+
+    <passport_no,contact_no,email_id,birth_registration,nid,licence> are the set of candidate key. 
+    We always consider primary key from the candidate key. after selecting the primary key, we 
+    consider the other key inside candidate set is called alternative keys.
+    
