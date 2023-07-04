f9146512f4979f9b5aca00fd30364953c27b87f7
ruhul <ra.01403288711@gmail.com>
2023-07-04 21:08:06 +0600

    Super key in mysql

---
+++ dbms/super_key_11.sql
@@ -0,0 +1,15 @@
+
+
+summation of candidate keys is called super key 
+
+Lets say a table has 5 columns . ContactNo,RegistrationNo,emailId,Age and dob 
+
+All these are candidate keys in the table 
+if i want to make a super key 
+
+the super key will be 
+
+superkey = ContactNo + emailId + RegistrationNo 
+
+To make a super key there must be at least one Candidate key with another column name. 
+
