d5811f011f38799f10b5b3ea2fee0fae443270ae
ruhul <ra.01403288711@gmail.com>
2023-07-07 12:02:34 +0600

    sql topics

---
+++ dbms/SQL/1.sql
@@ -0,0 +1,11 @@
+Topics
+-------
+1. sql is a domain specific language
+2. sql is a declarative language
+3. DDL, DML, DCL, TCL
+4.Operators(Like,between,IN,NOT IN, Conditional(<,>,<=,<>), Exist,Not Exist)
+5.Clauses(distinct,order by , group by , from , having)
+6. aggregate function(max,min,avg,sum)
+7.JOINS(Inner JOIN,LEFT,RIGHT)
+8.Sub Query
+9.PL SQL(function,cursor,procedures,triggers) (PL Stands for Procedural Language)
