25764517047192dceb0988684bef195ceccda18a
ruhul <ra.01403288711@gmail.com>
2023-07-07 12:11:04 +0600

    sql command

---
+++ dbms/SQL/2.sql
@@ -0,0 +1,11 @@
+Types of SQL:
+1. Data Defination Language 
+    Create,alter , drop,truncate,rename
+2. Data Manipulation Language
+    SELECT,Insert,update,delete 
+3. Data Control Language
+    Grant,Revoke
+4. Transaction Control Language 
+    Commit, roll back, save point 
+5. Constraints 
+    primary key, Foreign key, Check, Unique,Default, Not Null
