91d2fa10282c2d9f50ff83dbfac6aed2b9488e2d
ruhul <ra.01403288711@gmail.com>
2023-07-07 18:46:22 +0600

    constraints in mysql

---
+++ dbms/SQL/7.sql
@@ -0,0 +1,12 @@
+Constraints in SQL:
+1. Unique
+2. Not NULL
+3. Primary Key = Unique + Not NULL 
+4. check Constraints 
+5. Foreign Key (important for referantial integrity)
+6. Default 
+7. 
+
+
+
+
